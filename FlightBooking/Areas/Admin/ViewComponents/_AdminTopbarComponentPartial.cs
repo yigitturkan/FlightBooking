0493013fd8fe91156f5fedf81d5d02ad219587f3
Yiğit Türkkan <yigit.trkkan2003@gmail.com>
2026-04-17 00:00:14 +0300

    17.04.2026 Update 01

---
+++ FlightBooking/Areas/Admin/ViewComponents/_AdminTopbarComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlightBooking.Areas.Admin.ViewComponents
+{
+    public class _AdminTopbarComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ FlightBooking/Entities/Flight.cs
@@ -0,0 +1,21 @@
+﻿namespace FlightBooking.Entities
+{
+    public class Flight
+    {
+        public string FlightId { get; set; }
+        public string FlightNumber { get; set; }
+        public string AirlineCode { get; set; }
+        public string DepartureAirportCode { get; set; }
+        public string DepartureAirportName { get; set; }
+        public string ArrivalAirportCode { get; set; }
+        public string ArrivalAirportName { get; set; }
+        public DateTime DepartureTime { get; set; }
+        public DateTime ArrivalTime { get; set; }
+        public int DurationMinutes { get; set; }
+        public int TotalSeats { get; set; }
+        public int AvailableSeats { get; set; }
+        public decimal BasePrice { get; set; }
+        public string Currency { get; set; }
+        public string Status { get; set; }
+    }
+}
